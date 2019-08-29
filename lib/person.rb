84b69e9c0a5f3fb190905a441c6e718374527d1b
Ryan S <planetlucid@gmail.com>
2019-08-29 20:34:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,13 @@
 class Person
-  def initialize (name)
-  end  
+  def initialize(breed)
+    @breed = breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end 
 end
